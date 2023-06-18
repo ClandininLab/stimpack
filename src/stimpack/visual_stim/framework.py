d28c3c02c022935fcca516ac78772faa08018c8a
minseungchoi <minseung@stanford.edu>
2023-06-18 12:05:46 -0700

    visual_stim.framework remove fullscreen resizing

--- src/stimpack/visual_stim/framework.py
+++ src/stimpack/visual_stim/framework.py
@@ -46,11 +46,15 @@ class StimDisplay(QOpenGLWidget):
 
         # configure window to reside on a specific screen
         # re: https://stackoverflow.com/questions/6854947/how-to-display-a-window-on-a-secondary-display-in-pyqt
-        if screen.fullscreen:
-            desktop = QtWidgets.QDesktopWidget()
-            rectScreen = desktop.screenGeometry(screen.id)
-            self.move(rectScreen.left(), rectScreen.top())
-            self.resize(rectScreen.width(), rectScreen.height())
+        # if screen.fullscreen:
+        #     # qscreens = QtGui.QGuiApplication.screens()
+        #     # widget = QtWidgets.QWidget()
+        #     # widget.setScreen()
+
+        #     desktop = QtWidgets.QDesktopWidget()
+        #     rectScreen = desktop.screenGeometry(screen.id)
+        #     self.move(rectScreen.left(), rectScreen.top())
+        #     self.resize(rectScreen.width(), rectScreen.height())
 
         # stimulus initialization
         self.stim_list = []
