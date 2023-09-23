77f838ed929046e5dd602cceda6c1c5be252774e
Minseung Choi <minseung@stanford.edu>
2023-09-23 16:40:14 -0700

    KeytracManagers relative ctl, set_pos_0 override

--- src/stimpack/device/locomotion/loco_managers/keytrac_managers.py
+++ src/stimpack/device/locomotion/loco_managers/keytrac_managers.py
@@ -5,15 +5,14 @@ from stimpack.device.locomotion.loco_managers import LocoManager, LocoClosedLoop
 
 KEYTRAC_HOST = '127.0.0.1'  # The server's hostname or IP address
 KEYTRAC_PORT = 33335         # The port used by the server
-# PYTHON_BIN =   '/home/mc/venv/stimpack/bin/python'
-# KEYTRAC_PY =   '/home/mc/src/stimpack/src/stimpack/device/keytrac.py'
 PYTHON_BIN =   'python'
 KEYTRAC_PY =   'keytrac.py'
 
 class KeytracManager(LocoManager):
-    def __init__(self, python_bin=PYTHON_BIN, kt_py_fn=KEYTRAC_PY, start_at_init=True):
+    def __init__(self, python_bin=PYTHON_BIN, kt_py_fn=KEYTRAC_PY, relative_control=True, start_at_init=True):
         self.python_bin = python_bin
         self.kt_py_fn = kt_py_fn
+        self.relative_control = relative_control
         self.keytrac_host = KEYTRAC_HOST
         self.keytrac_port = KEYTRAC_PORT
 
@@ -27,7 +26,7 @@ class KeytracManager(LocoManager):
         if self.started:
             print("Keytrac is already running.")
         else:
-            self.p = subprocess.Popen([self.python_bin, self.kt_py_fn, self.keytrac_host, str(self.keytrac_port)], start_new_session=True)
+            self.p = subprocess.Popen([self.python_bin, self.kt_py_fn, self.keytrac_host, str(self.keytrac_port), self.relative_control], start_new_session=True)
             self.started = True
 
     def close(self, timeout=5):
@@ -47,9 +46,11 @@ class KeytracManager(LocoManager):
             print("Keytrac hasn't been started yet. Cannot be closed.")
 
 class KeytracClosedLoopManager(LocoClosedLoopManager):
-    def __init__(self, fs_manager, host=KEYTRAC_HOST, port=KEYTRAC_PORT, python_bin=PYTHON_BIN, kt_py_fn=KEYTRAC_PY, start_at_init=False, udp=True):
+    def __init__(self, fs_manager, host=KEYTRAC_HOST, port=KEYTRAC_PORT, 
+                       python_bin=PYTHON_BIN, kt_py_fn=KEYTRAC_PY, 
+                       relative_control=True, start_at_init=False, udp=True):
         super().__init__(fs_manager=fs_manager, host=host, port=port, save_directory=None, start_at_init=False, udp=udp)
-        self.kt_manager = KeytracManager(python_bin=python_bin, kt_py_fn=kt_py_fn, start_at_init=False)
+        self.kt_manager = KeytracManager(python_bin=python_bin, kt_py_fn=kt_py_fn, relative_control=relative_control, start_at_init=False)
 
         if start_at_init:    self.start()
 
@@ -66,6 +67,7 @@ class KeytracClosedLoopManager(LocoClosedLoopManager):
 
         # Keytrac lines always starts with KT
         if toks.pop(0) != "KT":
+            print(line)
             print('Bad read')
             return None
         
@@ -78,3 +80,11 @@ class KeytracClosedLoopManager(LocoClosedLoopManager):
         ts = float(toks[6])
 
         return {'theta': theta, 'x': x, 'y': y, 'z':z, 'frame_num': key_count, 'ts': ts}
+
+    def set_pos_0(self, theta_0=0, x_0=0, y_0=0, z_0=0, use_data_prev=True, get_most_recent=True, write_log=False):
+        self.socket_manager.send_message("reset_pos")
+        super().set_pos_0(theta_0=0, x_0=0, y_0=0, z_0=0, 
+                          use_data_prev=use_data_prev, 
+                          get_most_recent=get_most_recent, 
+                          write_log=write_log)
+        
