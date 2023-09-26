a73a66d583f757d311d3eb8be779da5ce2c1348e
Minseung Choi <minseung@stanford.edu>
2023-09-25 17:36:58 -0700

    experiment.server manager -> vis_stim_manager

--- src/stimpack/experiment/server.py
+++ src/stimpack/experiment/server.py
@@ -24,7 +24,7 @@ class BaseServer():
             self.port = port
 
         # other_stim_module_paths=[] stops StimServer from importing user stimuli modules from a txt file
-        self.manager = StimServer(screens=screens, host=self.host, port=self.port, auto_stop=False, other_stim_module_paths=[])
+        self.vis_stim_manager = StimServer(screens=screens, host=self.host, port=self.port, auto_stop=False, other_stim_module_paths=[])
 
         self.loco_manager = None
         self.daq_device = None
@@ -36,9 +36,9 @@ class BaseServer():
             assert issubclass(daq_class, DAQ)
             self.__set_up_daq__(daq_class, **daq_kwargs)
 
-        self.manager.corner_square_toggle_stop()
-        self.manager.corner_square_off()
-        self.manager.set_idle_background(0)
+        self.vis_stim_manager.corner_square_toggle_stop()
+        self.vis_stim_manager.corner_square_off()
+        self.vis_stim_manager.set_idle_background(0)
 
         def signal_handler(sig, frame):
             print('Closing server after Ctrl+C...')
@@ -50,31 +50,31 @@ class BaseServer():
             start_daemon_thread(self.loop)
 
     def loop(self):
-        self.manager.loop()
+        self.vis_stim_manager.loop()
 
     def close(self):
         if self.loco_manager is not None:
             self.loco_manager.close()
         if self.daq_device is not None:
             self.daq_device.close()
-        self.manager.shutdown_flag.set()
+        self.vis_stim_manager.shutdown_flag.set()
 
     def __set_up_loco__(self, loco_class, **kwargs):
-        self.loco_manager = loco_class(fs_manager=self.manager, start_at_init=False, **kwargs)
-        self.manager.register_function_on_root(self.loco_manager.set_save_directory, "loco_set_save_directory")
-        self.manager.register_function_on_root(self.loco_manager.start, "loco_start")
-        self.manager.register_function_on_root(self.loco_manager.close, "loco_close")
+        self.loco_manager = loco_class(fs_manager=self.vis_stim_manager, start_at_init=False, **kwargs)
+        self.vis_stim_manager.register_function_on_root(self.loco_manager.set_save_directory, "loco_set_save_directory")
+        self.vis_stim_manager.register_function_on_root(self.loco_manager.start, "loco_start")
+        self.vis_stim_manager.register_function_on_root(self.loco_manager.close, "loco_close")
         
         if issubclass(loco_class, LocoClosedLoopManager):
-            self.manager.register_function_on_root(self.loco_manager.set_pos_0, "loco_set_pos_0")
-            self.manager.register_function_on_root(self.loco_manager.write_to_log, "loco_write_to_log")
-            self.manager.register_function_on_root(self.loco_manager.loop_start, "loco_loop_start")
-            self.manager.register_function_on_root(self.loco_manager.loop_stop, "loco_loop_stop")
-            self.manager.register_function_on_root(self.loco_manager.loop_start_closed_loop, "loco_loop_start_closed_loop")
-            self.manager.register_function_on_root(self.loco_manager.loop_stop_closed_loop, "loco_loop_stop_closed_loop")
-            self.manager.register_function_on_root(self.loco_manager.loop_update_closed_loop_vars, "loco_loop_update_closed_loop_vars")
+            self.vis_stim_manager.register_function_on_root(self.loco_manager.set_pos_0, "loco_set_pos_0")
+            self.vis_stim_manager.register_function_on_root(self.loco_manager.write_to_log, "loco_write_to_log")
+            self.vis_stim_manager.register_function_on_root(self.loco_manager.loop_start, "loco_loop_start")
+            self.vis_stim_manager.register_function_on_root(self.loco_manager.loop_stop, "loco_loop_stop")
+            self.vis_stim_manager.register_function_on_root(self.loco_manager.loop_start_closed_loop, "loco_loop_start_closed_loop")
+            self.vis_stim_manager.register_function_on_root(self.loco_manager.loop_stop_closed_loop, "loco_loop_stop_closed_loop")
+            self.vis_stim_manager.register_function_on_root(self.loco_manager.loop_update_closed_loop_vars, "loco_loop_update_closed_loop_vars")
 
     def __set_up_daq__(self, daq_class, **kwargs):
         self.daq_device = daq_class(**kwargs)
-        self.manager.register_function_on_root(self.daq_device.send_trigger, "daq_send_trigger")
-        self.manager.register_function_on_root(self.daq_device.output_step, "daq_output_step")
+        self.vis_stim_manager.register_function_on_root(self.daq_device.send_trigger, "daq_send_trigger")
+        self.vis_stim_manager.register_function_on_root(self.daq_device.output_step, "daq_output_step")
