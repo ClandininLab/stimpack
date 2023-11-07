d9b38034eaaa3668895f46cc5af006a1871058ba
Minseung Choi <minseung@stanford.edu>
2023-11-07 12:24:25 -0800

    position -> state

--- src/stimpack/visual_stim/perspective.py
+++ src/stimpack/visual_stim/perspective.py
@@ -11,13 +11,13 @@ from stimpack.visual_stim.util import normalize, rotx, roty, rotz
 
 
 class GenPerspective:
-    def __init__(self, pa, pb, pc, pe=(0, 0, 0), near=0.0001, far=1000, fly_pos=(0, 0, 0), horizontal_flip=False):
+    def __init__(self, pa, pb, pc, pe=(0, 0, 0), near=0.0001, far=1000, subject_xyz=(0, 0, 0), horizontal_flip=False):
         # save settings
         self.pa = pa
         self.pb = pb
         self.pc = pc
         self.pe = pe
-        self.fly_pos = fly_pos
+        self.subject_xyz = subject_xyz
         self.near = near
         self.far = far
         self.horizontal_flip = horizontal_flip # for rear-projection display
@@ -29,7 +29,7 @@ class GenPerspective:
         pb = np.array(self.pb, dtype=float)
         pc = np.array(self.pc, dtype=float)
         pe = np.array(self.pe, dtype=float)
-        fly_pos = np.array(self.fly_pos, dtype=float)
+        subject_xyz = np.array(self.subject_xyz, dtype=float)
 
         # make aliases for "near" and "far" so that the code is easier to read
         n = self.near
@@ -66,21 +66,21 @@ class GenPerspective:
                       [vr[1], vu[1], vn[1], 0],
                       [vr[2], vu[2], vn[2], 0],
                       [    0,     0,     0, 1]], dtype=float)
-        T = np.array([[1, 0, 0, -fly_pos[0]],
-                      [0, 1, 0, -fly_pos[1]],
-                      [0, 0, 1, -fly_pos[2]],
+        T = np.array([[1, 0, 0, -subject_xyz[0]],
+                      [0, 1, 0, -subject_xyz[1]],
+                      [0, 0, 1, -subject_xyz[2]],
                       [0, 0, 0,      1]], dtype=float)
 
         return P.dot((M.T).dot(T)).astype('f4').tobytes(order='F')
 
     def rotx(self, th):
         return GenPerspective(pa=rotx(self.pa, th), pb=rotx(self.pb, th), pc=rotx(self.pc, th),
-                              pe=rotx(self.pe, th), near=self.near, far=self.far, fly_pos=self.fly_pos, horizontal_flip=self.horizontal_flip)
+                              pe=rotx(self.pe, th), near=self.near, far=self.far, subject_xyz=self.subject_xyz, horizontal_flip=self.horizontal_flip)
 
     def roty(self, th):
         return GenPerspective(pa=roty(self.pa, th), pb=roty(self.pb, th), pc=roty(self.pc, th),
-                              pe=roty(self.pe, th), near=self.near, far=self.far, fly_pos=self.fly_pos, horizontal_flip=self.horizontal_flip)
+                              pe=roty(self.pe, th), near=self.near, far=self.far, subject_xyz=self.subject_xyz, horizontal_flip=self.horizontal_flip)
 
     def rotz(self, th):
         return GenPerspective(pa=rotz(self.pa, th), pb=rotz(self.pb, th), pc=rotz(self.pc, th),
-                              pe=rotz(self.pe, th), near=self.near, far=self.far, fly_pos=self.fly_pos, horizontal_flip=self.horizontal_flip)
+                              pe=rotz(self.pe, th), near=self.near, far=self.far, subject_xyz=self.subject_xyz, horizontal_flip=self.horizontal_flip)
