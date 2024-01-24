bb803fe8286f8a49236c7f3a685658db2a585e10
Minseung Choi <minseung@stanford.edu>
2024-01-23 19:13:10 -0800

    examples.subscreens.py update

--- examples/visual_stim/subscreens.py
+++ examples/visual_stim/subscreens.py
@@ -2,6 +2,7 @@
 from stimpack.visual_stim.stim_server import launch_stim_server
 from stimpack.visual_stim.screen import Screen, SubScreen
 from stimpack.visual_stim.draw import draw_screens
+from stimpack.rpc.multicall import MyMultiCall
 
 from time import sleep
 
@@ -48,32 +49,55 @@ def get_subscreen(dir):
 
 
 def main():
-    subscreens = [get_subscreen('w'), get_subscreen('n'), get_subscreen('e')]
-    screen = Screen(subscreens=subscreens, fullscreen=True, square_loc=(0.75, -1.0), square_size=(0.25, 0.25), vsync=True, horizontal_flip=True)
 
+    # Set stimulus parameters
+    n_trials = 3
+    pre_time = 1 #sec
+    stim_time = 4 #sec
+    tail_time = 1 #sec
+    idle_background = (0.5, 0.5, 0.5, 1.0) #RGBA
+
+    # Initialize screens
+    subscreens = [get_subscreen('w'), get_subscreen('n'), get_subscreen('e')]
+    screen = Screen(subscreens=subscreens, fullscreen=False, square_loc=(0.75, -1.0), square_size=(0.25, 0.25), vsync=True, horizontal_flip=True)
     #draw_screens(screen)
 
+    # Initialize stim server
     manager = launch_stim_server(screen)
+    manager.set_idle_background(idle_background)
 
-    manager.load_stim(name='ConstantBackground', color=[0.5, 0.5, 0.5, 1.0], side_length=100)
+    for _ in range(n_trials):
+        # (1) LOAD STIMS
+        multicall_load = MyMultiCall(manager)
+        multicall_load.load_stim(name='ConstantBackground', color=idle_background, side_length=100)
 
-    manager.load_stim(name='RotatingGrating', rate=25, period=30, mean=0.5, contrast=1.0, offset=0.0, profile='square',
-                      color=[1, 1, 1, 1], cylinder_radius=1.1, cylinder_height=10, theta=0, phi=0, angle=90, hold=True)
+        multicall_load.load_stim(name='RotatingGrating', rate=25, period=30, mean=0.5, contrast=1.0, offset=0.0, profile='square',
+                        color=[1, 1, 1, 1], cylinder_radius=1.1, cylinder_height=10, theta=0, phi=0, angle=90, hold=True)
 
-    tv_pairs = [(0, 0), (4, 360)]
-    theta_traj = {'name': 'TVPairs',
-                  'tv_pairs': tv_pairs,
-                  'kind': 'linear'}
+        tv_pairs = [(0, 0), (4, 360)]
+        theta_traj = {'name': 'TVPairs',
+                    'tv_pairs': tv_pairs,
+                    'kind': 'linear'}
 
-    manager.load_stim(name='MovingPatch', width=30, height=30, phi=0, color=[1, 0, 0, 1], theta=theta_traj, hold=True, angle=0)
+        multicall_load.load_stim(name='MovingPatch', width=30, height=30, phi=0, color=[1, 0, 0, 1], theta=theta_traj, hold=True, angle=0)
 
-    sleep(0.5)
+        multicall_load()
+        sleep(pre_time)
 
-    manager.start_stim()
-    sleep(8)
+        # (2) START STIMS, FLICKER CORNER
+        multicall_start = MyMultiCall(manager)
+        multicall_start.start_stim()
+        multicall_start.corner_square_toggle_start()
+        multicall_start() #start stims
+        sleep(stim_time)
 
-    manager.stop_stim(print_profile=True)
-    sleep(0.5)
+        # (3) STOP STIMS, SET CORNER TO BLACK
+        multicall_stop = MyMultiCall(manager)
+        multicall_stop.stop_stim(print_profile=True)
+        multicall_stop.corner_square_toggle_stop()
+        multicall_stop.corner_square_off()
+        multicall_stop()
+        sleep(tail_time)
 
 if __name__ == '__main__':
     main()
