221c7f9029d466da96cfcbe16b53db92d11f95be
Minseung Choi <minseung@stanford.edu>
2024-05-11 12:16:22 -0700

    Updated version number to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stimpack',
-    version='0.1.0',
+    version='0.1.1',
     description='Precise and flexible generation of stimuli for neuroscience experiments.',
     url='https://github.com/ClandininLab/stimpack',
     author='Minseung Choi',
